d83f33a5a15990633f8d6bf86a2284aaf03dcc6e
Adamu Muhammad <sayyid211@gmail.com>
2022-11-30 19:14:31 +0000

    last digit

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def print_last_digit(number):
     if number < 0:
-        return((-1 * number) % 10)
+        ld = number % -10
+        ld *= -1
     else:
-        return(number % 10)
+        ld = number % 10
+    print("{:d}".format(ld), end='')
+    return(ld)
