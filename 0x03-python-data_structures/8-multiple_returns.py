c67074789d21cd9ec8c3387d7eaee174b5f0fba8
Adamu Muhammad <sayyid211@gmail.com>
2022-12-06 16:37:48 +0000

    mul rets

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence != ():
+    if len(sentence) > 0:
         length = len(sentence), sentence[0]
     else:
         length = 0, None
