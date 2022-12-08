61a43701aeccf709ff4c7748f33f95188d4c3456
Adamu Muhammad <sayyid211@gmail.com>
2022-12-08 03:33:40 +0000

    delete key in dict

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-   del(a_dictionary[key]) if a_dictionary[key] is not None
+   del(a_dictionary[key]) if a_dictionary.get(key) is not None
    return a_dictionary
