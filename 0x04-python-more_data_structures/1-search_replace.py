dc20c279439be2906e903824c81bf9633742f271
Adamu Muhammad <sayyid211@gmail.com>
2022-12-08 02:30:58 +0000

    replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    dup = my_list
+    dup = my_list[:]
     for i in range(len(my_list)):
-        if my_list[i] == search:
+        if dup[i] == search:
             dup[i] = replace
-    return dup
+        return dup
