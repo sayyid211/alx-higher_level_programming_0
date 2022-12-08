341d78ebac5541c1645b127203c8e2df1a644daa
Adamu Muhammad <sayyid211@gmail.com>
2022-12-08 03:58:52 +0000

    complex del

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+    keys = list(a_dictionary.keys))
+
+    for val in keys:
+        if value ==a_dictionary.get(val):
+            del a_dictionary[val]
+    return a_dictionary
