ebf84f2cb22a2eae875d50ae0e4f18158b786704
Adamu Muhammad <sayyid211@gmail.com>
2022-12-08 02:34:55 +0000

    replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,6 +2,6 @@
 def search_replace(my_list, search, replace):
     dup = my_list[:]
     for i in range(len(my_list)):
-        if dup[i] == search:
+        if my_list[i] == search:
             dup[i] = replace
-        return dup
+    return dup
