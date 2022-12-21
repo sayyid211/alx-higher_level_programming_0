e1ce0b41adcd42a0a4b3674284d4a21056f9cdf5
Adamu Muhammad <sayyid211@gmail.com>
2022-12-21 02:48:04 +0000

    4 - sqr

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+# Author: Adamu Muhammad
+"""Define square"""
+
+
+class Square:
+    """Square class"""
+    def __init__(self, size=0):
+                """initialize square
+        Args: size - of the aquare
+        Raises:
+            TypeError: size !int
+            ValurError: size < 0
+        """
+            if not isinstance(size, int):
+                raise TypeError('size must be an integer')
+            if size < 0:
+                raise ValueError('size must be >= 0')
+            self.__size = size
+
+    @property
+    def size(self):
+        """fetch square size"""
+
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if not instanceof(value, int):
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
+
+    def area(self):
+                """compute square area
+        Returns: area of square
+        """
+        area = self.__size * self.__size
+        return (area)
