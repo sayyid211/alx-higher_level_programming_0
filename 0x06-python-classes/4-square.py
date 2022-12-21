a3114a254f5985cb041bf146528ca550c2f835c7
Adamu Muhammad <sayyid211@gmail.com>
2022-12-21 09:05:06 +0000

    sqr 4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -26,7 +26,7 @@ class Square:
 
     @size.setter
     def size(self, value):
-        if not instanceof(value, int):
+        if not isinstance(value, int):
             raise TypeError('size must be an integer')
         if value < 0:
             raise ValueError('size must be >= 0')
