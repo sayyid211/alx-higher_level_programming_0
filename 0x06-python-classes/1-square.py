b7a7796e754259c35c2f7cc9d5eed0a4513a603f
Adamu Muhammad <sayyid211@gmail.com>
2022-12-21 01:48:27 +0000

    square with private size attrib

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Defines a Square"""
+
+
+class Square:
+
+    """a square class"""
+
+
+    def __init__(self, size):
+        """initialize square
+        Args: size - square size
+        """
+
+        self.__size = size
