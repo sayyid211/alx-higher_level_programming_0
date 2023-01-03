6219adeb6f0dfe9154ac737ce2cf3a5470cf041c
Adamu Muhammad <sayyid211@gmail.com>
2023-01-03 05:08:28 +0000

    3 rectangle

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -8,7 +8,7 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         """constructor"""
         self.__height = height
-        self.width = width
+        self.__width = width
 
     @property
     def width(self):
