cce57ca3caeb071bf182cf296e1d34a9614b50f3
Adamu Muhammad <sayyid211@gmail.com>
2023-01-09 15:39:29 +0000

    Add: Look up an object

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""lookup the attribs and methods of a class"""
+
+
+def lookup(obj):
+    """Looks up obj for attribs and methods"""
+
+    return (dir(obj))
---
+++ 0x0A-python-inheritance/README.md
@@ -0,0 +1,2 @@
+Inheritance
+Classes getting attributes from parent classes
