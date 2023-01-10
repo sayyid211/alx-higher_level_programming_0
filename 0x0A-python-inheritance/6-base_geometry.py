787be26e19f28cf4e45873e630b6d72bc8f4460f
Adamu Muhammad <sayyid211@gmail.com>
2023-01-10 03:27:09 +0000

    6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""base geometry class"""
+
+
+class BaseGeometry:
+    """base geometry class"""
+
+    def area(self):
+        """unimplemented area methon"""
+        raise Exception('area() is not implemented')
