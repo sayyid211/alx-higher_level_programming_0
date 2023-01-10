749c57deff7cc9319c233d9df6c33906402c20cd
Adamu Muhammad <sayyid211@gmail.com>
2023-01-10 02:24:13 +0000

    Add: 2 is_same_class

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Check if an object is an instance of class"""
+
+
+def is_same_class(obj, a_class):
+    """check if obj is a member of a_class"""
+
+    return (isinstance(obj, a_class))
