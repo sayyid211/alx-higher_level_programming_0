76c8c643aba8f511dd15908771823dcd04bda149
Adamu Muhammad <sayyid211@gmail.com>
2023-01-10 03:09:49 +0000

    4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -4,4 +4,4 @@
 
 def inherits_from(obj, a_class):
     """returns indirect descendance state of obj wrt a_class"""
-    return (issubclass(type(obj), a_class) and type(obj != a_class))
+    return (issubclass(type(obj), a_class) and type(obj) != a_class)))
