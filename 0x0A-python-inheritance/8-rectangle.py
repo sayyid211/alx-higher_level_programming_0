78306e1b5bf29814628ecaf5447924524b3a21c3
Adamu Muhammad <sayyid211@gmail.com>
2023-01-10 04:10:28 +0000

    8

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""module subclass rectangle"""
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """rectangle descendant og basegeometry"""
+
+    def __init__(self, width, height):
+        """class constructor"""
+
+        self.integer_validator('width', width)
+        self.integer_validator('height', height)
+
+        self.__width = width
+        self.__height = height
