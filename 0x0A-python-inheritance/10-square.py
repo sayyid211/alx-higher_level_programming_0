38e63c17833bb0b27b81fdde4feabb7eb1897962
Adamu Muhammad <sayyid211@gmail.com>
2023-01-11 00:20:47 +0000

    10

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""nested inheritance module"""
+
+
+Rextangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """child of rectangle"""
+
+    def __init__(self, size):
+        """constructor"""
+
+        self.integer_validator('size', size)
+
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        return (self.__size ** 2)
